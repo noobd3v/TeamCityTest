9f5632f203a84a816a954a603301ee30766e0850
Abhishek Chandel <abhishek.chandel@nagarro.com>
2017-09-03 13:15:30 +0530

    test trigger

--- TeamCityTest/Controllers/HomeController.cs
+++ TeamCityTest/Controllers/HomeController.cs
@@ -15,7 +15,7 @@ namespace TeamCityTest.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
+            ViewBag.Message = "Testing team city trigger";
 
             return View();
         }
